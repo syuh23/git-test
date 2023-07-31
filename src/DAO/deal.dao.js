f0c92d950fe069f0fbc7a157655134741d0c525d
syuh <seonghee5070@gmail.com>
2023-07-31 14:51:15 +0900

    deal_Post

--- src/DAO/deal.dao.js
+++ src/DAO/deal.dao.js
@@ -1,10 +1,16 @@
 const db = require('../config/db')
+const logger = require('../config/logger')
 
 function postDeal(postDeal_req) {
     return new Promise((resolve, reject) => {
-        var queryData = `select deal_id from deal where user_id = ${postDeal_req.user_id} and deal_name = ${postDeal_req.deal_name} and deal_content = ${postDeal_req.deal_content}`;
+        var queryData = `select deal_id from deal where user_id = ${postDeal_req.user_id} and deal_name = "${postDeal_req.deal_name}" and deal_content = "${postDeal_req.deal_content}"`;
         db.query(queryData, (error, db_data) => {
             if(error) {
+                logger.error(
+                    'DB error [deal]' +
+                    '\n \t' + queryData +
+                    '\n \t' + error
+                )
                 reject("DB ERR")
             }
             resolve(db_data)
--- src/controller/deal.controller.js
+++ src/controller/deal.controller.js
@@ -1,9 +1,19 @@
 const DealService = require('../service/deal.service');
 
 async function postDeal(req, res, next) {
-    const postDeal_req = req.body;
-    const postDeal_data = await DealService.postDeal(postDeal_req);
-    return res.json(postDeal_data)
+    try{
+        const postDeal_req = req.body;
+        console.log("1.", req.query);
+        console.log("2.", req.body);
+
+        const postDeal_data = await DealService.postDeal(postDeal_req);
+        return res.json(postDeal_data)
+    } catch (err){
+        console.log("실패..");
+    } finally {
+        console.log("ㅇㅅㅇ ㅠㅠ");
+    }
+
 }
 
 module.exports = {
